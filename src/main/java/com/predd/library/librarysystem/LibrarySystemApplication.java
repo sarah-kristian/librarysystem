3e01077275c1d7e376f0f3280f610e4010685026
Sarah Kristian Perry <sarah.perry@keyin.com>
2025-05-31 13:57:55 -0230

    Initial Spring Boot project setup

---
+++ src/main/java/com/predd/library/librarysystem/LibrarySystemApplication.java
@@ -0,0 +1,13 @@
+package com.predd.library.librarysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarySystemApplication.class, args);
+	}
+
+}
